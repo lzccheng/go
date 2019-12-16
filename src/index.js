6f4c65ca31a7be3b42c112d5e622bbc00a918226
lzccheng <1187350675@qq.com>
2019-12-16 16:59:27 +0800

    es6

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-function queryName (name) {
-    var search = location.search.slice(1)
-    var obj = {}
+const queryName = name => {
+    const search = location.search.slice(1)
+    const obj = {}
     search.split('&').forEach(function (i) {
         var arr = i.split('=')
         obj[arr[0]] = arr[1]
     })
     return obj[name]
 }
-var url = queryName('url')
+const url = queryName('url')
 location.href = url || `https://url.cn/5UzB4Tz`
